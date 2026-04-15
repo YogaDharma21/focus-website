26c2b8822207ee7a6ce44f0362895e737e641be9
Yoga Dharma <yogacode2006@gmail.com>
2026-04-15 08:15:03 +0800

    add vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -38,6 +39,7 @@ export default function RootLayout({
                     disableTransitionOnChange
                 >
                     {children}
+ <SpeedInsights />
                 </ThemeProvider>
             </body>
         </html>
